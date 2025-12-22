06c7af13ebe00d3f6fc46df9be7a3de31946fdb1
Beyonder <dalonvia@gmail.com>
2025-12-22 17:19:39 -0500

    actualizacion del .h para separar los tipos de datos

---
+++ include/estructuras.h
@@ -0,0 +1,45 @@
+#ifndef datos
+#define datos 
+// Contenido del header
+
+// ===== DATA STRUCTURES =====
+typedef enum {
+    TRANSACTION_INCOME,
+    TRANSACTION_EXPENSE
+} TransactionType;
+
+typedef struct {
+    TransactionType type;
+    float amount;
+    char description[128];
+    char date[32];
+} Transaction;
+
+typedef struct {
+    Transaction transactions[1000];
+    int count;
+    float totalIncome;
+    float totalExpense;
+    float balance;
+} Budget;
+
+typedef struct {
+    char amountInput[32];
+    char descriptionInput[128];
+    bool amountInputActive;
+    bool descriptionInputActive;
+    int selectedTransactionIndex;
+    char statusMessage[256];
+    float statusMessageTime;
+    char incomeText[64];
+    char expenseText[64];
+    char balanceText[64];
+} UIState;
+
+//CONSTANTES
+const int altura = 720;  // altura
+const int ancho = 1280;  // ancho
+
+
+
+#endif
--- src/main.c
+++ src/main.c
@@ -5,43 +5,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include "estructuras.h"
 
-const int altura = 720;  // altura = height
-const int ancho = 1280;  // ancho = width
-
-// ===== DATA STRUCTURES =====
-typedef enum {
-    TRANSACTION_INCOME,
-    TRANSACTION_EXPENSE
-} TransactionType;
-
-typedef struct {
-    TransactionType type;
-    float amount;
-    char description[128];
-    char date[32];
-} Transaction;
-
-typedef struct {
-    Transaction transactions[1000];
-    int count;
-    float totalIncome;
-    float totalExpense;
-    float balance;
-} Budget;
-
-typedef struct {
-    char amountInput[32];
-    char descriptionInput[128];
-    bool amountInputActive;
-    bool descriptionInputActive;
-    int selectedTransactionIndex;
-    char statusMessage[256];
-    float statusMessageTime;
-    char incomeText[64];
-    char expenseText[64];
-    char balanceText[64];
-} UIState;
 
 // ===== GLOBAL STATE =====
 Budget budget = {0};
